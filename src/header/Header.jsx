01fad525d45458707d4e9a462a50b2c560d381b7
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2023-01-20 00:04:30 +0530

    header

---
+++ src/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='Header-head-top-flex'>
+        <div>
+            <div>
+
+            </div>
+            <div>
+              FINCONIC 
+            </div>
+            your Iconic Banking Partner
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
