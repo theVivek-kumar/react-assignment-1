7be9130a8904a0d037609bbd81440e30ab1259ed
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2023-01-19 23:15:18 +0530

    initial-comit-project-setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <h1 class="text-5xl font-bold underline">
+    Hello world!
+  </h1>
+  <h1 class="text-6xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  );
+}
+
+export default App;
