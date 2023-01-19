8ee3513f9265f09564d4c9a89d8271ba9fbc7e0b
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2023-01-20 01:25:27 +0530

    navbar-done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1 class="text-3xl font-bold underline">
     Hello world!
-  </h1>
-  <h1 class="text-5xl font-bold underline">
-    Hello world!
-  </h1>
-  <h1 class="text-6xl font-bold underline">
-    Hello world!
-  </h1>
+  </h1> 
+      
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { AiOutlineDown } from 'react-icons/ai';
+
+function Header() {
+  return (
+    <div className='navbar-container'>
+        <div className="flex-justify-center">
+        <div>filcon</div>
+        <div className='flex-row' >
+            <div className='flex'>
+                Service <AiOutlineDown/>
+            </div>
+            <div>
+                About
+            </div>
+            <div className='flex'>
+                Tools <AiOutlineDown/>
+            </div>
+            <div>
+                Contact Us
+            </div>
+
+        </div>
+
+        <div  className='flex-row-signin'>
+            <div>SignIN</div>
+            <div>||</div>
+            <div>Signout</div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
